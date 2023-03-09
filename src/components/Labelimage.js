f82832875b4472f3b4b3e407a1e9dc0125205f0b
mikkopaa <mikkopaa@gmail.com>
2023-03-09 23:30:41 +0200

    Can now set labels to database

--- src/components/Labelimage.js
+++ src/components/Labelimage.js
@@ -2,57 +2,52 @@ import React, { useState } from 'react'
 import { Button } from '@mui/material'
 import Labelbutton from './Labelbutton'
 import { useParams } from 'react-router-dom'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 const Labelimage = () => {
     const { id } = useParams()
     const labels = ['roots', 'grease', 'cracks']
-    const labelObject = {
-            roots: false,
-            grease: false,
-            cracks: false
-        }
-
-    const [imageLabels, setImageLabels] = useState(labelObject)
+    const queryClient = useQueryClient()
 
     const fetchImage = async () => {
         const response = await axios.get(`http://localhost:8082/api/images/${id}`)
-        const image = response.data
-        setImageLabels(image.labels)
-        return image
+        return response.data
     }
 
-    // const updateImage = async (data) => {
-    //     console.log(data)
-    //     const updatedImage = { ...data, labels: ['roots']}
-    //     const response = await axios.put(`http://localhost:8082/api/images/${id}`, updatedImage)
-    //     return response.data
-    // }
+    const updateLabels = async (labels) => {
+        const response = await axios.put(`http://localhost:8082/api/images/${id}`, labels)
+        return response.data
+    }
+
+    const addOrRemove = (array, item) => {
+        const exists = array.includes(item)
+      
+        if (exists) {
+          return array.filter((c) => { return c !== item })
+        } else {
+          const result = array
+          result.push(item)
+          return result
+        }
+    }
 
     const { isError, isSuccess, isLoading, data, error } = useQuery(
-        ["image"],
-        fetchImage,
-        { staleTime: 60000 }
+        ['image'],
+        fetchImage
     )
+
+    const mutation = useMutation(updateLabels, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('image')
+        }
+    })
 
     if(isLoading){
         console.log("Loading...")
         return <div>loading...</div>
     }
-
-    // if(isSuccess) {
-    //     setImageLabels(data.labels)
-    // }
-
-    // const showLabels = () => {
-    //     if(isLoading) return "loading..."
-    //     else {
-    //         const labels = data.labels
-    //         return labels.toString()
-    //     }
-    // }
 
     const createButton = label => {
         return (
@@ -61,15 +56,15 @@ const Labelimage = () => {
     }
 
     const handleClick = (label) => {
-        const updatedLabels = imageLabels
-        updatedLabels[label] = !updatedLabels[label]
-        setImageLabels(labels => ({
-            ...labels,
-            ...updatedLabels 
-        }))
+        const newLabels = addOrRemove(data.labels, label)
+        mutation.mutate({
+            labels: newLabels
+        })
     }
 
-    // console.log("Rendering...")
+    const handleTransfer = () => {
+
+    }
 
     return (
         <div>
@@ -78,14 +73,12 @@ const Labelimage = () => {
                 Kuva {id}
             </div>
             <div>
-                {/* {data._id} */}
-                {/* {showLabels()} */}
-                {Object.entries(imageLabels).toString()}
+                {data.labels.toString()}
             </div>
             <div>
-                <Button variant='outlined' onClick={() => handleClick("previous")}>Previous</Button>
-                <Button variant='outlined' onClick={() => handleClick("next")}>Next</Button>
-                {labels.map(label => createButton(label, data))}
+                <Button variant='outlined' onClick={() => handleTransfer("next")}>Next</Button>
+                <Button variant='outlined' onClick={() => handleTransfer("previous")}>Previous</Button>
+                {labels.map(label => createButton(label))}
             </div>
         </div>
     );
